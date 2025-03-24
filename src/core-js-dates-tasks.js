788a35306a9ac45f1843383f38cfc38ae26e997d
Maxim Krestyaninov <maxipy@ya.ru>
2025-03-24 17:07:52 +0400

    feat: add getQuarter

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -242,8 +242,9 @@ function getNextFridayThe13th(/* date */) {
  * Date(2024, 5, 1) => 2
  * Date(2024, 10, 10) => 4
  */
-function getQuarter(/* date */) {
-  throw new Error('Not implemented');
+function getQuarter(date) {
+  const quarter = Math.floor(date.getMonth() / 3) + 1;
+  return quarter;
 }
 
 /**
